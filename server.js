edbf4e82a6aba2d7c307f1f8f1422e57e6c5f3e2
Fabio Ferreira Soares <fabio.soares@live.fr>
2019-05-11 14:33:04 +0200

    ser

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ const init = () => {
     server.use('/', mainRouter);
 
     //Lunch
-    server.listen(port, host, () => {
-        console.log(`Server is running on ${host}:${port}`)
+    server.listen( host, () => {
+        console.log(`Server is running on ${host}`)
     });
 };
 
